8f639037bececc956f405bb01ba879f6457b8f68
Ericlee1783 <ericlee1783@gmail.com>
2023-09-27 00:45:27 -0700

    commit

--- assets/styles.css/script.js
+++ assets/styles.css/script.js
@@ -1,11 +1,24 @@
 //declare document variables from html
 var searchBtnEl = $('#start');
+var cityEl = $('#search-form');
 
-
+console.log(cityEl)
+fetch("api.openweathermap.org/data/2.5/forecast?q=Boston&appid=8c99c2a4576e560d880726f5adda1776")
+ .then(function (response){
+    console.log(response);
+ })
 
 function startSearch () {
-    var weatherURL = "api.openweathermap.org/data/2.5/forecast?q=&appid=8c99c2a4576e560d880726f5adda1776"
+    var weatherURL = "api.openweathermap.org/data/2.5/forecast?q=&appid"
+    const options = {
+        method: 'GET',
+        headers: {
+            'API-Key': '8c99c2a4576e560d880726f5adda1776'
+        }
+    }
     console.log("api.openweathermap.org/data/2.5/forecast?q=Boston&appid=8c99c2a4576e560d880726f5adda1776")
+    console.log("api.openweathermap.org/data/2.5/forecast?q=&appid")
+    console.log(cityEl)
 }
 
 searchBtnEl.on('click', startSearch)

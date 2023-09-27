e1532cf67a5db844d48f52dd17b8bc17c82349b9
Ericlee1783 <ericlee1783@gmail.com>
2023-09-26 22:30:35 -0700

    added API key

--- assets/styles.css/script.js
+++ assets/styles.css/script.js
@@ -4,7 +4,8 @@ var searchBtnEl = $('#start');
 
 
 function startSearch () {
-    var weatherURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'
+    var weatherURL = "api.openweathermap.org/data/2.5/forecast?q=&appid=8c99c2a4576e560d880726f5adda1776"
+    console.log("api.openweathermap.org/data/2.5/forecast?q=Boston&appid=8c99c2a4576e560d880726f5adda1776")
 }
 
 searchBtnEl.on('click', startSearch)
